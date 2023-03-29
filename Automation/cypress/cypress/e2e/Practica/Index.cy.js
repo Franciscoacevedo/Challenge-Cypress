498aaeb27a620df86fee74392bf7cbf7720c3b73
franciscoacevedo <francisco031529@gmail.com>
2023-03-28 22:37:49 -0400

    Challenge Apap

---
+++ Automation/cypress/cypress/e2e/Practica/Index.cy.js
@@ -0,0 +1,53 @@
+/// <reference types="Cypress"/>
+
+import SesionPagina from '../../support/pages/PantallaLogin';
+import InputsPagina from '../../support/pages/PantallaInputs';
+import HoversPage from '../../support/pages/PantallaOVER';
+import DropdownPage from '../../support/pages/DropdownListPagina';
+
+describe('Challenge Apap', () => {
+
+
+    it('Credenciales Pantalla login ', () => {
+        cy.fixture('DATA').then((DATA) => {
+            SesionPagina.visit();
+        
+            DATA.loginUser[0].Users.forEach((user) => {
+                DATA.loginPass[0].Pass.forEach((pass) => {
+                    SesionPagina.login(user, pass);
+                })
+            })
+        })
+    });
+
+    it('Introducir numero para validar', () => {
+        cy.fixture('DATA').then((DATA) => {
+            InputsPagina.visit();
+            DATA.InputId[0].IDs.forEach((id) => {
+                InputsPagina.input(id);
+                InputsPagina.verify(id);
+            })
+        });
+    });
+
+    it('Over validar URL ', () => {
+        cy.fixture('DATA').then((DATA) => {
+            HoversPage.visit();
+            DATA.HoverId[0].IDs.forEach((id) => {
+                HoversPage.hoverOverAvatar(id);
+                HoversPage.verifyProfileLinkIsVisible(id);
+            })
+        })
+    });
+
+    it('Diferente dropdown list', () => {
+        cy.fixture('DATA').then((DATA) => {
+            DropdownPage.visit();
+            DATA.DropdownIDs[0].IDs.forEach((id) => {
+                DropdownPage.selectOption(id);
+                DropdownPage.verifySelectedOption(id);
+            })
+           
+        })
+    });
+});
